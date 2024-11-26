853695940da6893d58be0c5fbc85d278f534729c
peak <peak9035@gmail.com>
2024-11-26 15:43:07 +0800

    email send add

--- lib/send_email.py
+++ lib/send_email.py
@@ -1,78 +1,94 @@
 import smtplib
 from email.mime.text import MIMEText
-from email.header import Header
 from email.mime.multipart import MIMEMultipart
+
+# 邮件信息配置
+sender = '498431861@qq.com'
+# 设置客户端授权码，不是密码
+auth_code = 'dqljqxjwveqobihe'
+# smtp服务器配置
+smtp_server = 'smtp.qq.com'
+smtp_port = 587
+
+# 接收者邮箱 - 必须指定为字符串形式
+receiver_email = 'fhqqtd305@gmail.com,chenhan921226@gmail.com'
+
 
 class Email:
 
-    @classmethod
-    def send_email(cls):
-        # 邮件信息配置. 授权码 xdclass123
-        sender = '498431861@qq.com'
-        receiver = 'fhqqtd305@gmail.com'
-        auth_code = 'dqljqxjwveqobihe'  # 设置客户端授权码，不是密码
-
-        smtp_server = 'smtp.qq.com'
-        smtp_port = 587
+    def send_email(cls, body):
+        """
+        - 发送邮件
+        :param body: 普通字符串内容
+        :return: None
+        """
         subject = '自动化测试报告'
 
         # 定义发送内容
-        msg = MIMEText("hello", _subtype="plan", _charset="utf-8")
+        msg = MIMEMultipart()
         msg["Subject"] = subject  # 主题
         msg["From"] = sender  # 发件人
-        msg["To"] = receiver  # 收件人
+        msg.attach(MIMEText(body, 'plain'))
 
         # 拿到对象
-        smtp = smtplib.SMTP()
-        # 连接smtp服务器
-        smtp.connect(smtp_server, smtp_port)
+        smtp = smtplib.SMTP(smtp_server, smtp_port)
+        # 启动加密连接，连接smtp服务器
+        smtp.starttls()
         # 登录服务器,发件人和授权码
         smtp.login(sender, auth_code)
-        # 发送邮件，传入发送/接收者，消息内容
-        smtp.sendmail(sender, receiver, msg.as_string())
+
+        # 循环发送邮件，传入发送/接收者，消息内容
+        smtp.sendmail(sender, receiver_email.split(','), msg.as_string())
         # 退出
         smtp.quit()
+        print("All emails sent successfully.")
 
-    # @classmethod
-    # def send_test_report(cls):
-    #     # 邮件信息配置. 授权码 xdclass123
-    #     sender = '498431861@qq.com'
-    #     receiver = 'fhqqtd305@gmail.com'
-    #     auth_code = 'dqljqxjwveqobihe'  # 设置客户端授权码，不是密码
-    #
-    #     smtpserver = 'smtp.qq.com'
-    #     subject = '自动化测试报告'
-    #
-    #     # 读取文件内容
-    #     f = open("./result.html", 'rb')
-    #     mail_body = f.read()
-    #     f.close()
-    #
-    #     # HTML 形式的文件内容
-    #     html = MIMEText(mail_body, _subtype='html', _charset='utf-8')
-    #     html['Subject'] = subject
-    #     html['from'] = sender
-    #     html['to'] = receiver
-    #
-    #     # html附件 将测试报告放在附件中发送
-    #     att1 = MIMEText(mail_body, 'base64', 'gb2312')
-    #     att1["Content-Type"] = 'application/octet-stream'
-    #     att1["Content-Disposition"] = 'attachment; filename="XdclassTestReport.html"'  # 这里的filename可以任意写
-    #
-    #     msg = MIMEMultipart()
-    #     msg['Subject'] = subject  # 邮件的标题
-    #     msg.attach(html)  # 将html附加在msg里
-    #     msg.attach(att1)  # 新增一个附件
-    #
-    #
-    #     # 连接 登录 上smtp服务器
-    #     smtp = smtplib.SMTP()
-    #     smtp.connect('smtp.qq.com')
-    #     smtp.login(sender, auth_code)
-    #
-    #     # 发送邮件
-    #     smtp.sendmail(sender, receiver, msg.as_string())
-    #     smtp.quit()
+    def send_test_report(cls, report_path):
+        """
+        - 发送邮件 html报告及附件
+        :param report_path: html文件路径
+        :return: None
+        """
+        subject = '自动化测试报告'
+
+        # 读取文件内容
+        f = open(report_path, 'rb')
+        mail_body = f.read()
+        f.close()
+
+        # HTML 形式的文件内容
+        html = MIMEText(mail_body, _subtype='html', _charset='utf-8')
+        html['Subject'] = subject
+        html['From'] = sender
+        html["To"] = receiver_email
+
+        # html附件 将测试报告放在附件中发送
+        att1 = MIMEText(mail_body, 'base64', 'gb2312')
+        att1["Content-Type"] = 'application/octet-stream'
+        att1["Content-Disposition"] = 'attachment; filename="TestReport.html"'  # 这里的filename可以任意写
+
+        msg = MIMEMultipart()
+        msg['Subject'] = subject  # 邮件的标题
+        msg["From"] = sender  # 发件人
+        msg.attach(html)  # 将html附加在msg里
+        msg.attach(att1)  # 新增一个附件
+
+        # 拿到对象
+        smtp = smtplib.SMTP(smtp_server, smtp_port)
+        # 启动加密连接，连接smtp服务器
+        smtp.starttls()
+        # 登录服务器,发件人和授权码
+        smtp.login(sender, auth_code)
+
+        # 发送邮件
+        smtp.sendmail(sender, receiver_email.split(","), msg.as_string())
+        smtp.quit()
+        print("All emails sent successfully.")
+
 
 if __name__ == '__main__':
-    Email.send_email()
+    body = "test send email"
+    Email().send_email(body)
+
+    # report_path = "/Users/zhaofeng/Documents/workers/xingcuang/explorer_autotest/result.html"
+    # Email().send_test_report(report_path)
